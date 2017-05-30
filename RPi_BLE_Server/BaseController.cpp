93f5555a7e06db1984c431b4a5d977e566c3e449
Ludwig Gryffenberg <ludigryff@gmail.com>
2017-05-30 09:23:42 +0200

    Modified the server-side code to read packets containing LoRa data.

--- RPi_BLE_Server/BaseController.cpp
+++ RPi_BLE_Server/BaseController.cpp
@@ -20,7 +20,7 @@ BaseController::BaseController(const unsigned short int port)
 
     _isDone = false;
 
-    _isReady = false;    
+    _isReady = false;
     _isWaitingCount = 0;
     _hasWokenCount = 0;
 }
@@ -189,158 +189,69 @@ void BaseController::handleSession(const unsigned long int sessionID)
         {
             start = std::clock();
 
-            //std::stringstream bufferStream;
+            std::stringstream bufferStream;
 
-            //// REINTERPRET_CAST!
+            // REINTERPRET_CAST!
 
-            //bufferStream.write(reinterpret_cast<const char*>(buffer), bufferLength);
+            bufferStream.write(reinterpret_cast<const char*>(buffer), bufferLength);
 
-            //std::string currentLine;
+            std::string currentLine;
 
-            //std::getline(bufferStream, currentLine);
+            std::getline(bufferStream, currentLine);
 
-            //std::stringstream currentLineStream(currentLine);
+            std::stringstream currentLineStream(currentLine);
 
-            //std::string currentField;
+            std::string currentField;
 
-            //std::getline(currentLineStream, currentField, '&');
+            std::getline(currentLineStream, currentField, '&');
 
-            //std::string id = currentField.substr(currentField.find('=') + 1);
+            std::string id = currentField.substr(currentField.find('=') + 1);
 
-            //std::getline(currentLineStream, currentField, '&');
+            std::getline(currentLineStream, currentField, '&');
 
-            //unsigned long int timestamp = std::stoi(currentField.substr(currentField.find('=') + 1));
+            unsigned long int timestamp = std::stoi(currentField.substr(currentField.find('=') + 1));
 
-            //std::getline(currentLineStream, currentField, '&');
+            std::getline(currentLineStream, currentField, '&');
 
-            //std::string data = currentField.substr(currentField.find('=') + 1);
+            std::string data = currentField.substr(currentField.find('=') + 1);
 
-            //std::cout << data << std::endl;
+            std::cout << data << std::endl;
 
-            //std::getline(currentLineStream, currentField, '&');
+            std::getline(currentLineStream, currentField, '&');
 
-            //std::string station = currentField.substr(currentField.find('=') + 1);
+            std::string station = currentField.substr(currentField.find('=') + 1);
 
-            //std::cout << station << std::endl;
+            std::cout << station << std::endl;
 
-            //std::getline(currentLineStream, currentField, '&');
+            std::getline(currentLineStream, currentField, '&');
 
-            //float latitude = std::stof(currentField.substr(currentField.find('=') + 1));
+            float latitude = std::stof(currentField.substr(currentField.find('=') + 1));
 
-            //std::cout << latitude << std::endl;
+            std::cout << latitude << std::endl;
 
-            //std::getline(currentLineStream, currentField, '&');
+            std::getline(currentLineStream, currentField, '&');
 
-            //float longitude = std::stof(currentField.substr(currentField.find('=') + 1));
+            float longitude = std::stof(currentField.substr(currentField.find('=') + 1));
 
-            //std::cout << longitude << std::endl;
+            std::cout << longitude << std::endl;
 
-            //std::getline(currentLineStream, currentField, '&');
+            std::getline(currentLineStream, currentField, '&');
 
-            //float noiseAverage = std::stof(currentField.substr(currentField.find('=') + 1));
+            float noiseAverage = std::stof(currentField.substr(currentField.find('=') + 1));
 
-            //std::cout << noiseAverage << std::endl;
+            std::cout << noiseAverage << std::endl;
 
-            //std::getline(currentLineStream, currentField, '&');
+            std::getline(currentLineStream, currentField, '&');
 
-            //float rssi = std::stof(currentField.substr(currentField.find('=') + 1));
+            float rssi = std::stof(currentField.substr(currentField.find('=') + 1));
 
-            //std::cout << rssi << std::endl;
+            std::cout << rssi << std::endl;
 
-            //std::getline(currentLineStream, currentField, '&');
+            std::getline(currentLineStream, currentField, '&');
 
-            //unsigned long int sequenceNo = std::stoi(currentField.substr(currentField.find('=') + 1));
+            unsigned long int sequenceNo = std::stoi(currentField.substr(currentField.find('=') + 1));
 
-            //std::cout << sequenceNo << std::endl;
-
-            //try
-            //{
-            //    _mySQLControllerPtr->insertValuesRaw("5F87RF", 1493299361, "45854X2S5S12", "59172", -23.5, 12.0, 74.34, -187.00, 52);
-            //}
-
-            //catch (const sql::SQLException&)
-            //{
-            //    // Do nothing.
-            //}
-
-            long int currentIndex = 0;
-
-            while (currentIndex < bufferLength)
-            {
-                unsigned char messageLength;
-
-                unsigned char temperatureLength;
-                unsigned char humidityLength;
-                unsigned char batteryLength;
-
-                unsigned char conduitNameLength;
-
-                char id[6];
-
-                short int temperature;
-                unsigned short int humidity;
-                unsigned char battery;
-
-                char time[20];
-
-                char conduitName[50];
-
-                memcpy(static_cast<void*>(&messageLength), static_cast<const void*>(buffer + currentIndex), 1);
-
-                if ((static_cast<int>(messageLength) + 1) > bufferLength)
-                    break;
-
-                currentIndex += 1;
-                memcpy(static_cast<void*>(id), static_cast<const void*>(buffer + currentIndex), 6);
-
-                currentIndex += 6;
-                memcpy(static_cast<void*>(&temperatureLength), static_cast<const void*>(buffer + currentIndex), 1);
-                currentIndex += 1;
-                if (temperatureLength == sizeof(temperature))
-                    memcpy(static_cast<void*>(&temperature), static_cast<const void*>(buffer + currentIndex), temperatureLength);
-
-                currentIndex += temperatureLength;
-                memcpy(static_cast<void*>(&humidityLength), static_cast<const void*>(buffer + currentIndex), 1);
-                currentIndex += 1;
-                if (humidityLength == sizeof(humidity))
-                    memcpy(static_cast<void*>(&humidity), static_cast<const void*>(buffer + currentIndex), humidityLength);
-
-                currentIndex += humidityLength;
-                memcpy(static_cast<void*>(&batteryLength), static_cast<const void*>(buffer + currentIndex), 1);
-                currentIndex += 1;
-                if (batteryLength == sizeof(battery))
-                    memcpy(static_cast<void*>(&battery), static_cast<const void*>(buffer + currentIndex), batteryLength);
-
-                currentIndex += batteryLength;
-                memcpy(static_cast<void*>(time), static_cast<const void*>(buffer + currentIndex), 20);
-
-                currentIndex += 20;
-                memcpy(static_cast<void*>(&conduitNameLength), static_cast<const void*>(buffer + currentIndex), 1);
-                currentIndex += 1;
-                memcpy(static_cast<void*>(&conduitName), static_cast<const void*>(buffer + currentIndex), conduitNameLength);
-
-                std::stringstream idBuffer;
-
-                idBuffer << std::hex << std::uppercase;
-
-                for (unsigned int i = 0; i < 6; ++i)
-                    idBuffer << std::setfill('0') << std::setw(2) << static_cast<int>(id[i]);
-
-                std::string conduitNameString(conduitName, conduitNameLength);
-
-                std::ofstream fileLog("LOG_BLE.txt", std::ofstream::app);
-
-                if (fileLog.is_open())
-                {
-                    fileLog << conduitNameString << ": " << idBuffer.str() << "|" << temperature << "|" << humidity << "|" << static_cast<int>(battery) << "|" << time << std::endl;
-
-                    fileLog.close();
-                }
-
-                //std::cout << conduitNameString << ": " << idBuffer.str() << "|" << temperature << "|" << humidity << "|" << static_cast<int>(battery) << "|" << time << std::endl;
-
-                currentIndex += static_cast<int>(messageLength) + 1;
-            }
+            std::cout << sequenceNo << std::endl;
         }
 
         else
